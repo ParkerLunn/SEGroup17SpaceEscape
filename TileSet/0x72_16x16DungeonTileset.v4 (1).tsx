cc7e2fb9b6023c6299fdb88d643d520b71ba6e5d
Danilo Souza <danilos063@gmail.com>
2018-02-19 20:58:15 -0500

    Added Some Tiles, and TileSets
    Added Tile1, Tile2, Tile3
    Added some TileSets to be used on the Tiles

---
+++ TileSet/0x72_16x16DungeonTileset.v4 (1).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="0x72_16x16DungeonTileset.v4 (1)" tilewidth="10" tileheight="14" tilecount="1" columns="1">
+ <image source="../Downloads/0x72_16x16DungeonTileset.v4 (1).png" width="12" height="14"/>
+</tileset>
