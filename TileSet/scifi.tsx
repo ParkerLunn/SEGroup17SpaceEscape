3d133a3b517a0df16619cb8a8e88f3023a0bce89
Parker Lunn <parkerlunn@ufl.edu>
2018-02-25 16:21:23 -0500

    Added scifi tileset and one map

---
+++ TileSet/scifi.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="scifi" tilewidth="16" tileheight="16" tilecount="1326" columns="34">
+ <image source="schifi1.png" width="548" height="638"/>
+</tileset>
