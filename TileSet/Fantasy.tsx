98b2d53385fc9271be9ef1660b294eb4cbb336e8
Danilo Souza <danilos063@gmail.com>
2018-03-09 21:19:21 -0500

    New TileMaps

---
+++ TileSet/Fantasy.tsx
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Fantasy" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="../SEGroup17SpaceEscape-Map/TileSet/0x72_16x16DungeonTileset.v4.png" width="256" height="256"/>
+ <tile id="182">
+  <objectgroup draworder="index">
+   <object id="1" x="7.33333" y="5.66667" width="9" height="10.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="183">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="5.66667" width="9" height="10.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="184">
+  <objectgroup draworder="index">
+   <object id="1" x="4.66667" y="2.33333" width="11.3333" height="13.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="185">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="2.66667" width="10.6667" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="186">
+  <objectgroup draworder="index">
+   <object id="1" x="3" y="0.333333" width="13" height="15.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="187">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="15.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="190">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.333333" y="2.33333" width="16" height="13.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="191">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="2.33333" width="16" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="198">
+  <objectgroup draworder="index">
+   <object id="1" x="5.33333" y="0" width="10.6667" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="199">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.333333" width="10.3333" height="15.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="200">
+  <objectgroup draworder="index">
+   <object id="1" x="4.33333" y="-0.333333" width="11.6667" height="16.3333"/>
+  </objectgroup>
+ </tile>
+ <tile id="201">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.333333" y="-0.333333" width="12" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="202">
+  <objectgroup draworder="index">
+   <object id="1" x="0.333333" y="0" width="15.6667" height="15.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="203">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="15.6667" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ TileSet/SciFiClear.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="SciFiClear" tilewidth="16" tileheight="16" tilecount="1326" columns="34">
+ <image source="../SEGroup17SpaceEscape-Map/TileSet/schifi2.png" width="548" height="638"/>
+ <tile id="277">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.333333" width="15.6667" height="15.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="281">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.333333" width="15.6667" height="15.3333"/>
+  </objectgroup>
+ </tile>
+ <tile id="285">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="15.3333"/>
+  </objectgroup>
+ </tile>
+ <tile id="643">
+  <objectgroup draworder="index">
+   <object id="1" x="2.66667" y="0.333333" width="12.6667" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="645">
+  <objectgroup draworder="index">
+   <object id="1" x="3" y="-0.333333" width="12.6667" height="16.3333"/>
+  </objectgroup>
+ </tile>
+</tileset>
