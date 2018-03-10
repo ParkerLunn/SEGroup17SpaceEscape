87bc87c8fdeab26ecc46296c5ccb901bdb8f43ac
Danilo Souza <danilos063@gmail.com>
2018-03-09 21:21:55 -0500

    Scripts for Map Generation
    Unfinished

---
+++ Scripts/BoardManager.cs
@@ -0,0 +1,72 @@
+﻿//This script will procedurally generate the dungeon based on prefab rooms.
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+
+    [Serializable]
+    public class Count
+    {
+        public int minimun; //Min number of rooms in a floor
+        public int maximun; //Max number of rooms in a floor
+
+        public Count(int max, int min)
+        {
+            minimun = min;
+            maximun = max;
+        }
+    }
+
+    public int roomAmount; //amount of rooms this floor will have
+
+    public Count roomCount = new Count(1, 1); //used to get the random value for the amount of rooms
+
+    public List<GameObject> prefabs;
+
+    public List<Room> rooms; //array of gameObjects will hold all the prefab rooms that we will be able to spawn.
+
+    private Transform boardHolder; //Prefabs will be under it, will improve hierarchy readability
+
+    public List<int> choices;
+
+    void BoardSetup()
+    {
+        for (int i = 1; i < roomAmount; i++)
+        {
+            int choice = Random.Range(1, roomAmount);
+            choices.Add(choice);
+
+            if (i == 1)
+                Instantiate(rooms[choice].room, rooms[choice].npos, Quaternion.identity).transform.SetParent(boardHolder);
+
+            else {
+                rooms[choice].npos.y += rooms[choices[i - 2]].npos.y;
+                Instantiate(rooms[choice].room, rooms[choice].npos, Quaternion.identity).transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+	public void SetUp()
+    {
+        boardHolder = new GameObject("Board").transform;
+
+        roomAmount = Random.Range(roomCount.minimun, roomCount.maximun + 1);
+
+        for (int i = 0; i < roomAmount; i++)
+        {
+            rooms.Add(ScriptableObject.CreateInstance<Room>());
+            rooms[i].SetRoom(prefabs[0]);
+            prefabs.RemoveAt(0);
+        }
+
+        Vector3 temp = Vector3.zero;
+        temp.x = -rooms[0].room.transform.Find("Transition").Find("DoorNorth").localPosition.x;
+
+        Instantiate(rooms[0].room, temp, Quaternion.identity).transform.SetParent(boardHolder);
+
+        BoardSetup();
+    }
+}
---
+++ Scripts/Room.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room: ScriptableObject{
+
+    public GameObject room;
+
+    public bool NDoor, WDoor, SDoor, EDoor;
+
+    public Vector3 npos, spos, wpos, epos;
+
+    public void SetRoom(GameObject _room)
+    {
+        room = _room;
+        HasDoor();
+        SetPosition();
+    }
+
+
+
+    void HasDoor()
+    {
+        if (room.transform.Find("Transition").Find("DoorNorth") != null)
+        {
+            NDoor = true;
+        }
+
+        else NDoor = false;
+
+        if (room.transform.Find("Transition").Find("DoorSouth") != null)
+        {
+            SDoor = true;
+        }
+
+        else SDoor = false;
+
+        if (room.transform.Find("Transition").Find("DoorEast") != null)
+        {
+            EDoor = true;
+        }
+
+        else EDoor = false;
+
+        if (room.transform.Find("Transition").Find("DoorWest") != null)
+        {
+            WDoor = true;
+        }
+
+        else WDoor = false;
+    }
+
+    private void SetPosition()
+    {
+        if (NDoor)
+        {
+            npos.y = room.GetComponent<Tiled2Unity.TiledMap>().GetMapHeightInPixelsScaled();
+            npos.x = -room.transform.Find("Transition").Find("DoorNorth").localPosition.x;
+        }
+
+        if (WDoor)
+        {
+            wpos.x = -room.GetComponent<Tiled2Unity.TiledMap>().GetMapWidthInPixelsScaled();
+        }
+
+        if (EDoor)
+        {
+            epos.x = room.GetComponent<Tiled2Unity.TiledMap>().GetMapWidthInPixelsScaled();
+        }
+    }
+}
