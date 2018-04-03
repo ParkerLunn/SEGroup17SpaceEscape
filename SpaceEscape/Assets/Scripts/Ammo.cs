e1a776c42c3724047d4b93baf2c7ff1252f896bd
Gianne <gianneflores18@gmail.com>
2018-04-03 11:25:28 -0400

    Inventory commit

---
+++ SpaceEscape/Assets/Scripts/Ammo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Ammo : ItemBaseClass {
+
+//	public override string Name {
+//		get{ 
+//			return "Ammo";
+//		}
+//	}		
+}
