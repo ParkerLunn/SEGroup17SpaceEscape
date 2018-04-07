5dcf4b5d7eace3da35582b2e493d4ad9fa96d52f
jeckolb <jeckolb@ufl.edu>
2018-04-07 09:33:15 -0400

    Updated menu stuff, backing up before trying to link inventory

---
+++ SpaceEscape/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public string newGame;
+	public string loadGame;
+
+	public void NewGame() 
+	{
+		Application.LoadLevel (newGame);
+	}
+
+	public void LoadGame() 
+	{
+		//Put code to load last save state in here
+	}
+
+	public void QuitGame() 
+	{
+		Application.Quit ();
+	}
+}
