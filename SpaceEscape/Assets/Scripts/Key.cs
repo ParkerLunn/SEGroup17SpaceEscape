bc5cd605da1861f8f1ab05d258427c9ccec4ed0e
jeckolb <jeckolb@ufl.edu>
2018-04-15 18:02:22 -0400

    Added wall switches, moveable crates, and puzzle doors. Puzzle room 1 is done for the most part

--- SpaceEscape/Assets/Scripts/Key.cs
+++ SpaceEscape/Assets/Scripts/Key.cs
@@ -2,19 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Key : MonoBehaviour {
-    MonoBehaviour Lock;
-
-	// Use this for initialization
-	void Start () {	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    void Use()
-    {
-
-    }
+public class Key : ItemBaseClass {
+   
 }
---
+++ SpaceEscape/Assets/Scripts/wallScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallScript : MonoBehaviour {
+
+	bool isOn = false;
+	public GameObject switchOn;
+	public GameObject switchOff;
+	public GameObject doorOpen;
+	public GameObject doorClosed;
+
+	public void TurnSwitchOn() 
+	{
+		isOn = true;
+		switchOn.SetActive (true);
+		switchOff.SetActive (false);
+		doorOpen.SetActive (true);
+		doorClosed.SetActive (false);
+	}
+
+	public void TurnSwitchOff()
+	{
+		isOn = false;
+		switchOn.SetActive (false);
+		switchOff.SetActive (true);
+		doorOpen.SetActive (false);
+		doorClosed.SetActive (true);
+	}
+}
