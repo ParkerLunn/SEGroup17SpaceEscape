a57f6404868aa5761881194a118ff96254fcff84
Numbuh474 <35499003+Numbuh474@users.noreply.github.com>
2018-04-16 19:58:03 -0400

    Switch Update
    Added additional type of switch.
    Fixed Door animation.

--- SpaceEscape/Assets/Scripts/ToggleSwitch.cs
+++ SpaceEscape/Assets/Scripts/ToggleSwitch.cs
@@ -2,34 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Switch : MonoBehaviour {
+public class ToggleSwitch : MonoBehaviour {
+
     public GameObject Lock;
     LockObj myLock;
     Rigidbody2D rb2d;
     bool activated;
     float timer;
     Animator animator;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         myLock = Lock.GetComponent("LockObj") as LockObj;
         rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         activated = false;
         timer = -1;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            myLock.Unlock();
-            activated = false;
+            myLock.Toggle();
+            activated = !activated;
             animator.SetBool("active", activated);
         }
     }
-
 }
