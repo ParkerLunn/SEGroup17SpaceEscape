b7fac9bda16e07fddc7cd2049dffe772f95af4e1
Numbuh474 <35499003+Numbuh474@users.noreply.github.com>
2018-04-16 21:44:07 -0400

    Added sounds to door opening and closing

--- SpaceEscape/Assets/Scripts/LockObj.cs
+++ SpaceEscape/Assets/Scripts/LockObj.cs
@@ -1,35 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class LockObj : MonoBehaviour
 {
     MonoBehaviour key;
     public bool locked;
+    public AudioClip doorOpenSound;
+    public AudioClip doorCloseSound;
     Animator animator;
+    AudioSource audioSource;
     // Use this for initialization
     void Start()
     {
         key = GetComponent<MonoBehaviour>();
         locked = true;
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
         animator.SetBool("locked", locked);
+
+        doorOpenSound = (AudioClip)AssetDatabase.LoadAssetAtPath("Assets/Sounds/door open.wav", typeof(AudioClip));
+        doorCloseSound = (AudioClip)AssetDatabase.LoadAssetAtPath("Assets/Sounds/door close.wav", typeof(AudioClip));
     }
 
     public void Unlock()
     {
-        //key.SendMessage("Use");
-        locked = false;
-        GetComponent<Collider2D>().enabled = locked; 
-        animator.SetBool("locked", locked);
+        if (locked == true)
+        {
+            //key.SendMessage("Use");
+            locked = false;
+            GetComponent<Collider2D>().enabled = locked;
+            animator.SetBool("locked", locked);
+            audioSource.clip = doorOpenSound;
+            audioSource.Play();
+        }
 
     }
     public void Lock()
     {
-        //key.SendMessage("Use");
-        locked = true;
-        GetComponent<Collider2D>().enabled = locked;
-        animator.SetBool("locked", locked);
+        if (locked == false)
+        {
+            //key.SendMessage("Use");
+            locked = true;
+            GetComponent<Collider2D>().enabled = locked;
+            animator.SetBool("locked", locked);
+            audioSource.clip = doorCloseSound;
+            audioSource.Play();
+        }
     }
     public void Toggle()
     {
