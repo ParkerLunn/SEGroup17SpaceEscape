d7ed41a118fb871c9eff36c661cecb3932af8e8b
Robert <robert.beck12569@gmail.com>
2018-04-16 23:10:18 -0400

    Added some new scripts to support projectiles and damaging/healing entities

---
+++ SpaceEscape/Assets/Scripts/Entity/PlayerPickUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPickUp : MonoBehaviour {
+
+	public Inventory inventory;
+
+	private void OnTriggerEnter2D(Collider2D other){ 
+		InteractableItemBaseClass item = other.GetComponent<InteractableItemBaseClass> (); //sets var item into object player ran into
+		if (item != null) { 
+			inventory.AddItem (item as ItemBaseClass); // adds item into inventory 
+			Debug.Log ("Adding: " + item);
+		}
+	}
+}
---
+++ SpaceEscape/Assets/Scripts/Items/ClickItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickItem : MonoBehaviour {
+
+	public Inventory inventory;
+
+	public void onItemClick(){
+		ItemDrag itemDrag = gameObject.transform.Find ("ItemImage").GetComponent<ItemDrag>();
+
+		ItemBaseClass item = itemDrag.Item;
+
+		Debug.Log ("CLICK" + item);
+		inventory.UseItem (item);
+		//		item.OnUse();
+	}
+}
