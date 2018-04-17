520697c8e95778437407c9b568a94b8a814b2674
Danilo Souza <danilos063@gmail.com>
2018-04-17 02:12:35 -0400

    Created the Save class

---
+++ SpaceEscape/Assets/Scripts/Save.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class Save{
+    public static List<Game> saves = new List<Game>();
+
+	public static void SaveGame()
+    {
+        saves.Add(GameObject.current);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/savedGames.spe");
+        bf.Serialize(file, saves);
+        file.Close();
+    }
+}
